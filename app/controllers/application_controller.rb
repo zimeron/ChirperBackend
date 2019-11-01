448166ee15396f1f362b10c33d1815754c8422fe
Andrew Fisher <fishera11@xavier.edu>
2019-11-01 15:58:55 -0400

    We test in production (tm)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::API
-    skip_before_action :verify_authenticity_token
 
     include ActionController::Cookies
-    include ActionController::RequestForgeryProtection
 
-    protect_from_forgery with: :exception
 
     def current_user
         if session[:user_id]
